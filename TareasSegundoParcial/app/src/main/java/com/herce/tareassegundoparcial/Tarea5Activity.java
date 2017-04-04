363b0c48237e88b2413bf557c8da278b9d42356f
Alejandro Herce <alejandro@herce.me>
2017-04-04 02:04:08 -0500

    Creado tareas 2 parcial

---
+++ TareasSegundoParcial/app/src/main/java/com/herce/tareassegundoparcial/Tarea5Activity.java
@@ -0,0 +1,13 @@
+package com.herce.tareassegundoparcial;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Tarea5Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tarea5);
+    }
+}
